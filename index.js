3d8bad74d275dd9f439e2234aa3cecc65bcb4d1f
Julia Saari <mariajulia.saari@gmail.com>
2020-10-23 10:17:10 +0300

    ex3.2

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+let phonenumbers = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Phonebook</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonenumbers)
+})
+
+app.get('/info', (request, response) => {
+    const numCount = phonenumbers.length
+    response.send(`<p>Phonebook has info for ${phonenumbers.length} people</p> <br/> ${Date()}`)
+})
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
