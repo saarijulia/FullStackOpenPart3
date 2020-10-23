0d3d321b29affc458c1a49390c8310d3b51eae43
Julia Saari <mariajulia.saari@gmail.com>
2020-10-23 10:31:04 +0300

    ex3.3

--- index.js
+++ index.js
@@ -32,9 +32,21 @@ app.get('/api/persons', (request, response) => {
     response.json(phonenumbers)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonenumbers.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+    
+})
+
 app.get('/info', (request, response) => {
     const numCount = phonenumbers.length
-    response.send(`<p>Phonebook has info for ${phonenumbers.length} people</p> <br/> ${Date()}`)
+    response.send(`<p>Phonebook has info for ${numCount} people</p> <br/> ${Date()}`)
 })
 const PORT = 3001
 app.listen(PORT, () => {
