ca5170c6b2fc634a408347a235dffa50e831b962
Julia Saari <mariajulia.saari@gmail.com>
2020-11-02 09:21:42 +0200

    deploy on heroku

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { request } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
@@ -95,7 +97,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
